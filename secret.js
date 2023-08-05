01e631a3c5ab54591c045860b71ef80915244fea
bijenderbisht <113984589+bijenderbisht@users.noreply.github.com>
2023-08-05 14:17:37 +0530

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,4 +1,4 @@
 Access key= zaCELgL.0imfnc8mVLWwsAawjYr4Rx-Af50DDqtlx
-secret key ='sdkfkhg768687asdfas676987jkhlkjhk'
+Secret key ='sdkfkhg768687asdfas676987jkhlkjhk'
 Access keys = AKIAIOSFODNN7EXAMPLE
 secret access key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
